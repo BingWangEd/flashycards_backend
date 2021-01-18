7319958b2274ae1ff39af365a077c2e67e87b9c9
Bing Wang <bwang01@uchicago.edu>
2021-01-18 20:35:50 +0900

    deploy change 2

--- build/src/class/Game.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Game = void 0;
-var Game = /** @class */ (function () {
-    function Game() {
-    }
-    return Game;
-}());
-exports.Game = Game;
